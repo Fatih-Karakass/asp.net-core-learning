f21a5de36081f0232f6521cd1ddb09dcb8f52b39
Fatih Karakaş <kfatih2744@gmail.com>
2022-10-13 20:55:52 +0300

    proje starting

---
+++ Ders1/Ders1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ders1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
